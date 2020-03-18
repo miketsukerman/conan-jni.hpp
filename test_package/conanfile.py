d233ee86857e0801773c7f676e8d2628e607b9ed
Michael Tsukerman <miketsukerman@gmail.com>
2020-03-18 21:45:42 +0100

    add -fPIC flag for gcc version 7 or lower

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -10,11 +10,19 @@ class TestPackageConan(ConanFile):
     generators = "cmake"
     requires = "java_installer/9.0.0@bincrafters/stable"
 
-    def build(self):
+    def configure_cmake(self):
         cmake = CMake(self)
+        if self.settings.compiler == "gcc" and \
+            float(self.settings.compiler.version.value) <= 7:
+                cmake.definitions["CMAKE_CXX_FLAGS"] = "-fPIC"
+        return cmake
+
+    def build(self):
+        cmake = self.configure_cmake()
         cmake.configure()
         cmake.build()
 
     def test(self):
         with tools.environment_append(RunEnvironment(self).vars):
             bin_path = os.path.join("lib", "libtest_package.so")
+        
